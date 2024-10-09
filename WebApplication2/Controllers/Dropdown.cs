31ccf2a68f7c38fca3b2c08b0d0c72e59d7b150f
Harleen <Harleen@DESKTOP-SVU35M7>
2024-10-09 11:13:42 +0530

    dropdown_change

---
+++ WebApplication2/Controllers/Dropdown.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class Dropdown : Controller
+    {
+        public IActionResult Drop()
+        {
+            return View();
+        }
+    }
+}
